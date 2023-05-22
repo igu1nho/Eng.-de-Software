14f76e0bbb4121b83752e0a09080e29622f40729
Igor Luiz Rodrigues <89806466+igu1nho@users.noreply.github.com>
2023-05-22 16:53:55 -0300

    Add files via upload

---
+++ Tarefa_Strategy_Observer/Observer/src/main/java/br/inatel/cdg/Valve.java
@@ -0,0 +1,51 @@
+package br.inatel.cdg;
+
+import br.inatel.cdg.palavra.impl.Observavel;
+import br.inatel.cdg.palavra.impl.Observador;
+
+public class Valve {
+
+	public static void main(String[] args) {
+
+		// Criando o observavel (subject) palavras.
+		Observavel palavras = new Observavel();
+
+		//Criando 6 observadores e fazendo a inscrição em noticias do CS2.
+		Observador obs1 = new Observador(1);
+		Observador obs2 = new Observador(2);
+		Observador obs3 = new Observador(3);
+		Observador obs4 = new Observador(4);
+		Observador obs5 = new Observador(5);
+		Observador obs6 = new Observador(6);
+
+		palavras.registraObservador(obs1);
+		palavras.registraObservador(obs2);
+		palavras.registraObservador(obs3);
+		palavras.registraObservador(obs4);
+		palavras.registraObservador(obs5);
+		palavras.registraObservador(obs6);
+
+		String frase = "O CS2 estara disponivel a partir do dia vinte e um de agosto";
+
+		palavras.contPalavra(frase);
+		palavras.contPalavraPar(frase);
+		palavras.contPalavrasMaiuscula(frase);
+
+		System.out.println("Observadores serao notificados");
+		palavras.notificaObservadores();
+
+		// O observador 6 deseja-se desinscrever, pois agora ele joga Valorant
+		palavras.removeObservador(obs6);
+
+		// Dia de lancamento do jogo chegou
+		String frase2 = "A nova atualizacao do CS2 ja esta disponivel. Aproveite!";
+
+		palavras.contPalavra(frase2);
+		palavras.contPalavraPar(frase2);
+		palavras.contPalavrasMaiuscula(frase2);
+
+		System.out.println("Observadores serao notificados");
+		palavras.notificaObservadores();
+	}
+
+}
